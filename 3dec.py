d499939a132ffedb1c6523013821c0e864dc365d
Matilda Andersson <ma4748an-s@student.lu.se>
2020-12-03 21:52:09 +0100

    Added alternative solution

--- 3dec.py
+++ 3dec.py
@@ -38,5 +38,22 @@ def part2():
 
     print(math.prod(counter))
 
-part1()
-part2()
+
+def part2_alt(stride, every_second=False):
+    cnt, second_loop = 0,0
+    i = stride
+    for line in input[1:]:
+        if line[i] == '#' and not every_second:
+            cnt += 1
+        if second_loop and every_second:
+            if line[i] == '#':
+                cnt += 1
+            i = (i+1) % 31
+        i = (i+stride) % 31
+        second_loop = (second_loop+1) % 2
+    return cnt
+
+counter_alt = [0]*5
+for i, stride in enumerate([1,3,5,7,1]):
+    counter_alt[i] = part2_alt(stride) if not i == 4 else part2_alt(stride, True)
+print(math.prod(counter_alt))
