03d2376861faac0c9998d383192c21bbdd082f62
Matilda Andersson <ma4748an-s@student.lu.se>
2020-12-15 21:58:37 +0100

    Updated code

--- 15dec.py
+++ 15dec.py
@@ -3,7 +3,8 @@ mem, spoken = {}, 0
 for i, num in enumerate(input[:-1]):
     mem[num] = i+1
 
-for turn in range(7,30000002):
+p2 = False
+for turn in range(7,(30000002 if p2 else 2022)):
     if spoken in mem.keys():
         tmp = spoken
         spoken = (turn-1) - mem[spoken]
@@ -13,5 +14,5 @@ for turn in range(7,30000002):
         spoken = 0 
     
 for k in mem.keys():
-    if mem[k] == 30000000:
+    if mem[k] == (30000000 if p2 else 2020):
         print(k)
